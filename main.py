8ad42b10ac84bad1a090348e4fa9756b2772d047
Mikhail Lapshin <urbancell022@gmail.com>
2023-04-06 23:12:40 +0700

    Start Py education

---
+++ main.py
@@ -0,0 +1,64 @@
+# a = 7
+# b = 5.34
+# c = 'hello'
+
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a, b, c))
+
+
+# print("Input first number")
+# a = input()
+# b = input('Input second number')
+
+# print(a, ' + ', b, " = ", a+b)
+
+
+# c = 5.89
+# print(c + 5)
+# print(type(c))
+# c = str(c)
+# print(c + "89")
+# print(type(c))
+
+
+# print("Input first number: ")
+# a = int(input())
+# b = int(input('Input second number'))
+
+# print(a, ' + ', b, " = ", a + b)
+
+
+# a = 5.3444
+# b = 4.232
+# print(round(a*b, 2))
+
+
+# username = input("Input NAME: ")
+# if username == 'Маша':
+#   print("EEEEE, Mary")
+# elif username == 'Марина':
+#   print("Oo yeeee!")
+# elif username == 'Илья':
+#   print('Salam')
+# else:
+#   print('HelloMoto', username)
+
+
+# n = int(input('Input numb: '))
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:  # если остаток при делении числа n на i равен 0
+#         flag = False
+#         print(i)
+#     elif i > n // 2:  # делитель числа не может превышать введенное число, деленное на 2
+#         print(n)
+#         flag = False
+#     i += 1
+
+
+# r = range(100, 0, -10)
+# for i in r:
+#   print(i)
+
+
