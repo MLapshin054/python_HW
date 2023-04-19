67d59e60398ec8d25d74a9d6a1b10247e711e746
Mikhail Lapshin <urbancell022@gmail.com>
2023-04-19 13:24:46 +0700

    Update 04 Seminar

--- seminar04.py
+++ seminar04.py
@@ -8,29 +8,81 @@
 # Для решения данной задачи используйте функцию
 # .split()
 
-testList = ('a a a b c a a d c d d').split()
-print(type(testList))
-slovar = {}
-result = None
-for i in testList:
-  if i in slovar:
-    print(f'{i}_{slovar[i]}')
-    slovar[i] += 1
-  else:
-    print(f'{i}')
-    slovar[i]=1
-    
+# testList = ('a a a b c a a d c d d').split()
+# print(type(testList))
+# slovar = {}
+# result = None
+# for i in testList:
+#     if i in slovar:
+#         print(f'{i}_{slovar[i]}')
+#         slovar[i] += 1
+#     else:
+#         print(f'{i}')
+#         slovar[i] = 1
 
-string = "a a a b c a a d c d d"
-my_list = string.split()
-my_dict = dict()
 
-for i in range(len(my_list)):
-    if my_list[i] in my_dict.keys():
-        my_dict[my_list[i]] += 1
-        my_list[i] = f"{my_list[i]}_{my_dict[my_list[i]]}"
-    else:
-        my_dict[my_list[i]] = 0
+# string = "a a a b c a a d c d d"
+# my_list = string.split()
+# my_dict = dict()
 
-print(my_list)
-    
+# for i in range(len(my_list)):
+#     if my_list[i] in my_dict.keys():
+#         my_dict[my_list[i]] += 1
+#         my_list[i] = f"{my_list[i]}_{my_dict[my_list[i]]}"
+#     else:
+#         my_dict[my_list[i]] = 0
+
+# print(*my_list)
+
+
+# Задача №27. Решение в группах
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
+# Output: 13
+
+
+input_text = """She sells sea shells on the sea shore The shells
+that she sells are sea shells I'm sure So if she sells sea
+shells on the sea shore I'm sure that the shells are sea
+shore shells"""
+
+print(set(input_text.split()))
+print(len(set(input_text.lower().split())))
+
+
+# Задача №29
+# Ваня и Петя поспорили, кто быстрее решит
+# следующую задачу: “Задана последовательность
+# неотрицательных целых чисел. Требуется определить
+# значение наибольшего элемента
+# последовательности, которая завершается первым
+# встретившимся нулем (число 0 не входит в
+# последовательность)”. Однако 2 друга оказались не
+# такими смышлеными. Никто из ребят не смог до
+# конца сделать это задание. Они решили так: у кого
+# будет меньше ошибок в коде, тот и выиграл спор. За
+# помощью товарищи обратились к Вам, студентам.
+# Примечание: Программные коды на следующих
+# слайдах
+
+number = int(input('Введите число: '))
+maxNumber = number
+while number != 0:
+    number = int(input('Введите число: '))
+    if number > maxNumber:
+        maxNumber = number
+print(f'Максимальное число: {maxNumber}!')
+
+# С использованием моржового оператора
+maxx = -1
+while (number := int(input('Введите число: '))) != 0:
+    if number > maxx:
+        maxx = number
+print('Максиммальное число -> {}'.format(maxx))
