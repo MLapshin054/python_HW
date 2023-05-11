58005f1c61dbaa5441e6501bab9df709a9429887
Mikhail Lapshin <urbancell022@gmail.com>
2023-05-11 21:43:50 +0700

    Add HW06. Task32

--- HW06/task30.py
+++ HW06/task30.py
@@ -12,3 +12,10 @@ sequence = []
 for i in range(1, quantity + 1):
     sequence.append(first_element + (i - 1) * step)
 print(sequence)
+
+
+# a1 = int(input())
+# d = int(input())
+# n = int(input())
+# for i in range(n):
+#   print(a1 + i * d)
--- HW06/task32.py
+++ HW06/task32.py
@@ -2,3 +2,20 @@
 # диапазону(т.е. не меньше заданного минимума и не больше заданного максимума)
 
 
+import random
+
+list_a = [random.randint(-15, 15) for _ in range(10)]
+print(list_a)
+min_value = -7
+max_value = 7
+for i in range(len(list_a)):
+    if min_value < list_a[i] < max_value:
+        print(i, end=' ')
+
+
+# list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# min_number = int(input())
+# max_number = int(input())
+# for i in range(len(list_1)):
+#     if min_number <= list_1[i] <= max_number:
+#         print(i)
